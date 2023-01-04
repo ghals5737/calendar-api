2ca1c78f1b96010c606b3e6d072ea4d08b096f28
ghals5737 <52785761+ghals5737@users.noreply.github.com>
2023-01-04 17:07:06 +0900

    Update gh_deploy.sh

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PROJECT_NAME="github_action"
-JAR_PATH="/home/ubuntu/github_action/calendar/build/libs/*.jar"
+JAR_PATH="/home/ubuntu/github_action/calendar/build/libs/calendar-0.0.1-SNAPSHOT.jar"
 DEPLOY_PATH=/home/ubuntu/$PROJECT_NAME/
 DEPLOY_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy.log"
 DEPLOY_ERR_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy_err.log"
