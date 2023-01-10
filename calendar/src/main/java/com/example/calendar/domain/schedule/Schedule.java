f7220ce32f59b2450d0c2a8b267acbcf7820684f
aaa <ghals5737@naver.com>
2023-01-10 11:20:07 +0900

    [test]:add selectScheduleById Test

---
+++ calendar/src/main/java/com/example/calendar/domain/schedule/Schedule.java
@@ -0,0 +1,18 @@
+package com.example.calendar.domain.schedule;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Schedule {
+
+}
---
+++ calendar/src/test/java/com/example/calendar/service/schedule/ScheduleServiceTest.java
@@ -0,0 +1,27 @@
+package com.example.calendar.service.schedule;
+
+import lombok.NoArgsConstructor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@SpringBootTest
+@NoArgsConstructor
+public class ScheduleServiceTest {
+    @Autowired
+    private ScheduleService scheduleService;
+
+    @Test
+    @DisplayName("schedule_id로 schedule 조회가 정상 작동한다.")
+    void selectScheduleByIdTest(){
+        //given
+        Long scheduleId=1L;
+        SelectScheduleByIdResponse expect=SelectScheduleByIdResponse().builder().build();
+        //when
+        SelectScheduleByIdResponse result=scheduleService.selectScheduleById(scheduleId);
+        //then
+        assertThat(result.getTitle()).isEqualTo(expect.getTitle());
+    }
+}
