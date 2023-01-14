c73e8b59f9de309d9e68d56f8a22a471d875c5b6
homin <ghals5737@naver.com>
2023-01-14 23:55:59 +0900

    [feat]:schedule 생성 컨트롤러 로직 생성

--- calendar/src/test/java/com/example/calendar/controller/schedule/ScheduleControllerTest.java
+++ calendar/src/test/java/com/example/calendar/controller/schedule/ScheduleControllerTest.java
@@ -1,7 +1,9 @@
 package com.example.calendar.controller.schedule;
 
 import com.example.calendar.domain.schedule.Schedule;
+import com.example.calendar.dto.schedule.request.CreateScheduleRequest;
 import com.example.calendar.repository.schedule.ScheduleRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -33,6 +35,9 @@ public class ScheduleControllerTest {
     private MockMvc mockMvc;
 
     @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
     private ScheduleRepository scheduleRepository;
 
     @AfterEach
@@ -43,33 +48,44 @@ public class ScheduleControllerTest {
     @Test
     @DisplayName("스케쥴생성 API 정상동작 확인")
     public void createScheduleTest() throws Exception{
+        //given
+        CreateScheduleRequest request= CreateScheduleRequest.builder()
+                .calendarId(1L)
+                .title("test")
+                .startDt(LocalDateTime.now())
+                .endDt(LocalDateTime.now())
+                .des("test des")
+                .color("test color")
+                .build();
 
+        //when,then
         this.mockMvc.perform(
                         RestDocumentationRequestBuilders
-                                .post("/api/schedule")
-                                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(document("schedule-api",
-                        requestFields(
-                                fieldWithPath("calendarId").description("달력 아이디"),
-                                fieldWithPath("title").description("일정 제목"),
-                                fieldWithPath("startDt").description("일정 시작일"),
-                                fieldWithPath("endDt").description("일정 종료일"),
-                                fieldWithPath("des").description("일정 상세"),
-                                fieldWithPath("color").description("일정 색")
-                        ),
-                        responseFields(
-                                fieldWithPath("headers").description("해더 정보"),
-                                fieldWithPath("body.result").description("API 실행결과정보"),
-                                fieldWithPath("body.data.scheduleId").description("일정 ID"),
-                                fieldWithPath("body.data.title").description("제목"),
-                                fieldWithPath("body.data.startDt").description("시작일"),
-                                fieldWithPath("body.data.endDt").description("종료일"),
-                                fieldWithPath("body.data.des").description("상세 설명"),
-                                fieldWithPath("body.data.color").description("색깔"),
-                                fieldWithPath("statusCode").description("http status 상태코드"),
-                                fieldWithPath("statusCodeValue").description("http status 상태숫자코드")
-                        )));
+                            .post("/api/schedule")
+                            .content(objectMapper.writeValueAsString(request))
+                            .accept(MediaType.APPLICATION_JSON))
+                            .andExpect(status().isOk())
+                            .andDo(document("schedule-api",
+                                    requestFields(
+                                            fieldWithPath("calendarId").description("달력 아이디"),
+                                            fieldWithPath("title").description("일정 제목"),
+                                            fieldWithPath("startDt").description("일정 시작일"),
+                                            fieldWithPath("endDt").description("일정 종료일"),
+                                            fieldWithPath("des").description("일정 상세"),
+                                            fieldWithPath("color").description("일정 색")
+                                    ),
+                                    responseFields(
+                                            fieldWithPath("headers").description("해더 정보"),
+                                            fieldWithPath("body.result").description("API 실행결과정보"),
+                                            fieldWithPath("body.data.scheduleId").description("일정 ID"),
+                                            fieldWithPath("body.data.title").description("제목"),
+                                            fieldWithPath("body.data.startDt").description("시작일"),
+                                            fieldWithPath("body.data.endDt").description("종료일"),
+                                            fieldWithPath("body.data.des").description("상세 설명"),
+                                            fieldWithPath("body.data.color").description("색깔"),
+                                            fieldWithPath("statusCode").description("http status 상태코드"),
+                                            fieldWithPath("statusCodeValue").description("http status 상태숫자코드")
+                                    )));
     }
 
     @Test
