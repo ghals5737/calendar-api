4c7078f1b15ce068c76afb95b800d82218ac63fc
ulms <ghals5737@naver.com>
2023-02-22 10:16:07 +0900

    [fix]:cors 보안설정 수정

--- calendar/src/main/java/com/example/calendar/config/WebConfig.java
+++ calendar/src/main/java/com/example/calendar/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("POST", "GET", "PUT", "DELETE","OPTIONS")
                 .allowedHeaders("Content-Type", "Authorization", "X-Requested-With", "Custom-Header")
                 .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Methods", "Access-Control-Allow-Headers", "Access-Control-Max-Age")
-                .allowCredentials(false)
+                .allowCredentials(true)
                 .maxAge(3600);
     }
 }
