56667c55b19eeb8a205ef781eceaba6552420f8a
MINI <52785761+ghals5737@users.noreply.github.com>
2023-02-22 23:48:54 +0900

    Update gh_deploy.sh

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -1,35 +1,58 @@
 #!/bin/bash
-PROJECT_NAME="github_action"
-JAR_PATH="/home/ubuntu/github_action/calendar/build/libs/calendar-0.0.1-SNAPSHOT.jar"
-DEPLOY_PATH=/home/ubuntu/$PROJECT_NAME/
-DEPLOY_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy.log"
-DEPLOY_ERR_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy_err.log"
-APPLICATION_LOG_PATH="/home/ubuntu/$PROJECT_NAME/application.log"
 
+# 프로젝트 이름과 배포 경로를 설정합니다.
+project_name="github_action"
+deploy_path="/home/ubuntu/$project_name"
 
-echo "===== 배포 시작 : $(date +%c) =====" >> $DEPLOY_LOG_PATH
+# 배포에 사용할 JAR 파일의 경로와 이름을 설정합니다.
+jar_path="$deploy_path/calendar/build/libs/calendar-0.0.1-SNAPSHOT.jar"
 
-# echo "> build 파일명: $JAR_NAME" >> $DEPLOY_LOG_PATH
-# echo "> build 파일 복사" >> $DEPLOY_LOG_PATH
-# cp $BUILD_JAR $DEPLOY_PATH
+# 로그 파일 경로를 설정합니다.
+deploy_log_path="$deploy_path/deploy.log"
+deploy_err_log_path="$deploy_path/deploy_err.log"
+application_log_path="$deploy_path/application.log"
 
-# echo "> 현재 동작중인 어플리케이션 pid 체크" >> $DEPLOY_LOG_PATH
-# CURRENT_PID=$(pgrep -f $JAR_NAME)
+# 이전 어플리케이션의 PID를 저장할 변수를 초기화합니다.
+current_pid=""
 
+echo "===== 배포 시작 : $(date +%c) =====" >> "$deploy_log_path"
 
+# 현재 동작중인 어플리케이션의 PID를 찾습니다.
+current_pid=$(pgrep -f "$jar_path")
 
-if [ -z $CURRENT_PID ]
+if [ -z "$current_pid" ]
 then
-  echo "> 현재 동작중인 어플리케이션 존재 X" >> $DEPLOY_LOG_PATH
+  # 이전 어플리케이션이 없을 경우, 적절한 로그를 출력합니다.
+  echo "> 현재 동작중인 어플리케이션이 없습니다." >> "$deploy_log_path"
 else
-  echo "> 현재 동작중인 어플리케이션 존재 O" >> $DEPLOY_LOG_PATH
-  echo "> 현재 동작중인 어플리케이션 강제 종료 진행" >> $DEPLOY_LOG_PATH
-  echo "> fuser -k 8080/tcp 
+  # 이전 어플리케이션을 종료합니다.
+  echo "> 이전 어플리케이션 (PID: $current_pid)을 종료합니다." >> "$deploy_log_path"
+  kill "$current_pid"
+
+  # 이전 어플리케이션의 종료를 기다립니다.
+  echo "> 이전 어플리케이션 종료를 기다립니다." >> "$deploy_log_path"
+  sleep 10
+
+  # 이전 어플리케이션 종료 여부를 확인합니다.
+  current_pid=$(pgrep -f "$jar_path")
+  if [ -n "$current_pid" ]
+  then
+    echo "> 이전 어플리케이션이 종료되지 않았습니다." >> "$deploy_log_path"
+    exit 1
+  fi
 fi
 
-echo "> DEPLOY_JAR 배포" >> $DEPLOY_LOG_PATH
-nohup java -jar $JAR_PATH  >> $APPLICATION_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
+# 새로운 어플리케이션을 배포합니다.
+echo "> 새로운 어플리케이션 배포를 시작합니다." >> "$deploy_log_path"
+nohup java -jar "$jar_path" >> "$application_log_path" 2>> "$deploy_err_log_path" &
 
-sleep 3
+# 어플리케이션 실행을 기다립니다.
+echo "> 어플리케이션 실행을 기다립니다." >> "$deploy_log_path"
+sleep 10
 
-echo "> 배포 종료 : $(date +%c)" >> $DEPLOY_LOG_PATH
+# 어플리케이션 실행 여부를 확인합니다.
+current_pid=$(pgrep -f "$jar_path")
+if [ -n "$current_pid" ]
+then
+  echo "> 어플리케이션이 성공적으로 실행되었습니다. (PID: $current_pid)" >> "$deploy_log_path"
+  echo "> 배포가 완료되었습니다. : $(date +%c)" >> "$deploy_log_path"
