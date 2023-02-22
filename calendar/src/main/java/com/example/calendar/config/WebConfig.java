8da7e757173a48a37d84889c1d02f91d1abcb895
ulms <ghals5737@naver.com>
2023-02-22 10:22:27 +0900

    [fix]:cors 보안설정 수정

--- calendar/src/main/java/com/example/calendar/config/WebConfig.java
+++ calendar/src/main/java/com/example/calendar/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://minical.vercel.app")
                 .allowedMethods("POST", "GET", "PUT", "DELETE","OPTIONS")
                 .allowedHeaders("Content-Type", "Authorization", "X-Requested-With", "Custom-Header")
                 .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Methods", "Access-Control-Allow-Headers", "Access-Control-Max-Age")
